74f73461ad0bdebd5da0d572e416a4418f32595d
Kevin Otten <kevin.otten@ziggo.nl>
2018-02-09 16:35:22 +0100

    Alles van menu -de options menu

---
+++ IndianaBones/Assets/GameFiles/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		GameManager.ChangeScene(1);
+	}
+
+	public void Options()
+	{
+
+	}
+
+	public void Quit()
+	{
+		GameManager.Quit();
+	}
+}
