ab8aa513276b13a85b49bb22ecd46ca825256441
Kevin Otten <kevin.otten@ziggo.nl>
2018-02-09 17:13:41 +0100

    pause menu naast options klaar

---
+++ IndianaBones/Assets/GameFiles/Scripts/UiManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour {
+
+	public GameObject pauseMenuPannel;
+
+	void Update()
+	{
+		if(Input.GetButtonDown("ESC") && !pauseMenuPannel.activeSelf)
+		{
+			print("ok");
+			pauseMenuPannel.SetActive(true);
+			GameManager.ToggleTimeScale();
+		}
+		else if(Input.GetButtonDown("ESC") && pauseMenuPannel.activeSelf)
+		{
+			print("ok2");
+			pauseMenuPannel.SetActive(false);
+			GameManager.ToggleTimeScale();
+		}
+	}
+	public void Resume()
+	{
+		pauseMenuPannel.SetActive(false);
+		GameManager.ToggleTimeScale();
+	}
+	public void Options()
+	{
+
+	}
+	public void Quit()
+	{
+		GameManager.Quit();
+	}
+	
+
+	
+}
