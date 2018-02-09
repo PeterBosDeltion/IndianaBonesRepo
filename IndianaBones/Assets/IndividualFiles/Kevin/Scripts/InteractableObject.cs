0b78e74d7ee8d26e5fe733418d8e6ce7ad9dfa75
Kevin Otten <kevin.otten@ziggo.nl>
2018-02-09 15:53:50 +0100

    Scriptable objects en triggerdobjects en daarbij bijhoorende scripts klaar/premade

---
+++ IndianaBones/Assets/IndividualFiles/Kevin/Scripts/InteractableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+
+	public TriggerdObjects toTrigger;
+
+	public void Trigger()
+	{
+		toTrigger.TriggerFunctionality();
+	}
+
+	public void OnCollisionStay(Collision collision)
+	{
+		if(collision.transform.tag == "Player")
+		{
+			if(Input.GetButtonDown("E"))
+			{
+				Trigger();
+			}
+		}
+	}
+}
---
+++ IndianaBones/Assets/IndividualFiles/Kevin/Scripts/TriggerdObjects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerdObjects : MonoBehaviour {
+
+	public virtual void TriggerFunctionality()
+	{
+		
+	}
+}
