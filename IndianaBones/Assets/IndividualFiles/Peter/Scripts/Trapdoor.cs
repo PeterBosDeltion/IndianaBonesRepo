6995a8292f09e2d1a3187a9e2e9c044e8516225d
gmd1apeterbos <97040824@st.deltion.nl>
2018-03-08 10:08:42 +0100

    Trapdoor

--- IndianaBones/Assets/IndividualFiles/Peter/Scripts/Trapdoor.cs
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/Trapdoor.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Trapdoor : TriggerdObjects {
-
+    public Animator anim;
 	// Use this for initialization
 	void Start () {
-		
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +16,7 @@ public class Trapdoor : TriggerdObjects {
 
     public override void TriggerFunctionality()
     {
-        gameObject.SetActive(false);
+        anim.Play("TrapdoorOpen");
+        //gameObject.SetActive(false);
     }
 }
