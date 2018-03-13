15423d2805bc26f0e64e675b813025232a3c3976
gmd1apeterbos <97040824@st.deltion.nl>
2018-03-13 10:49:21 +0100

    Memeic

--- IndianaBones/Assets/IndividualFiles/Peter/Scripts/Mimic.cs
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/Mimic.cs
@@ -7,15 +7,20 @@ public class Mimic : TriggerdObjects {
 	public override void TriggerFunctionality()
 	{
 
-        GetComponent<Animator>().Play("Mimic");
-        clip = GetComponent<AnimationClip>();
-        Debug.Log(clip.name);
+        clip = GetComponentInParent<Animation>().clip;
+        GetComponentInParent<Animation>().Play();
+        StartCoroutine(WaitforAnim());
 
     }
 
     IEnumerator WaitforAnim()
     {
+        GameObject p = GameObject.FindGameObjectWithTag("Player");
+        p.GetComponent<PlayerMovement>().enabled = false;
         yield return new WaitForSeconds(clip.length);
+        p.GetComponent<Player>().Death();
+        p.GetComponent<PlayerMovement>().enabled = true;
+
         //Player ded
     }
 }
