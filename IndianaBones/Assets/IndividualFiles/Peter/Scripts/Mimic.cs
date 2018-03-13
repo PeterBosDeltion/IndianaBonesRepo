dc1bfb8465a74097a918cf64a12cf1bf9b5be0b2
gmd1apeterbos <97040824@st.deltion.nl>
2018-03-13 10:02:07 +0100

    Mimic work

---
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/Mimic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mimic : TriggerdObjects {
+    private AnimationClip clip;
+	public override void TriggerFunctionality()
+	{
+
+        GetComponent<Animator>().Play("Mimic");
+        clip = GetComponent<AnimationClip>();
+        Debug.Log(clip.name);
+
+    }
+
+    IEnumerator WaitforAnim()
+    {
+        yield return new WaitForSeconds(clip.length);
+        //Player ded
+    }
+}
