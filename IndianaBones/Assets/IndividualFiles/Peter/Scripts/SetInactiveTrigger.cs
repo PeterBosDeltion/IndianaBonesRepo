1d4319c26c4f1c1a270eac37f5a5e5fc19e3b157
gmd1apeterbos <97040824@st.deltion.nl>
2018-03-16 15:36:54 +0100

    Traps r gay

---
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/SetInactiveTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetInactiveTrigger : TriggerdObjects {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void TriggerFunctionality()
+    {
+        gameObject.SetActive(false);
+    }
+}
