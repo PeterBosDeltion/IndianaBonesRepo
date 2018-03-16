e9f9443380a246f2b4c91b858cbc51f84f7e9a99
Kevin Otten <kevin.otten@ziggo.nl>
2018-03-16 17:05:32 +0100

    updateje

---
+++ IndianaBones/Assets/GameFiles/Scripts/Puzzles/LightsOff/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : TriggerdObjects {
+	
+	void Start()
+	{
+		puzzleManager = GameObject.FindObjectOfType<PuzzleManager>();
+	}
+	public override void TriggerFunctionality()
+	{
+		//GetComponent<Animator>().SetTrigger("Push");
+		if(puzzleNumber != 0)
+		{
+			if(puzzleManager.puzzleList[puzzleNumber].puzzleDone == false)
+        	{
+           		puzzleManager.puzzleInsert(this);
+        	}
+		}
+	}
+}
---
+++ IndianaBones/Assets/GameFiles/Scripts/Puzzles/LightsOff/TurnLightsOffPuzzle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLightsOffPuzzle : Puzzle {
+
+	public int gridWidth;
+	public GameObject[,] lights;
+	public int currentIndex = 0;
+	public int hor = 0;
+	public int ver = 0;
+
+
+	public List<bool> lightsOnOff = new List<bool>();
+
+	void Start()
+	{
+		puzzleManager = GameObject.FindObjectOfType<PuzzleManager>();
+		lights = new GameObject[gridWidth,gridWidth];
+	}
+
+	public override bool PuzzleTrigger(TriggerdObjects currentObject)
+	{
+		if(currentObject.puzzlePart == 1)
+		{
+			hor -= 1;
+		}
+		if(currentObject.puzzlePart == 2)
+		{
+			lights[hor,ver].GetComponent<Lights>().toggleLights;
+		}
+		if(currentObject.puzzlePart == 3)
+		{
+			hor += 1;
+		}
+		if(currentObject.puzzlePart == 4)
+		{
+			ver -= 1;
+		}
+		return CheckLights(currentObject.puzzlePart);
+	}
+
+	public bool CheckLights(int trigger)
+	{
+		returnBool = true;
+		if(lightsOnOff[trigger] ==false)
+		{
+			lightsOnOff[trigger] = true;
+		}
+		else
+		{
+			lightsOnOff[trigger] = false;
+		}
+		foreach (bool light in lightsOnOff)
+		{
+			if(light == false)
+			{
+				returnBool = false;
+			}
+		}
+		puzzleManager.triggers -= 1;
+		return returnBool;
+	}
+}
