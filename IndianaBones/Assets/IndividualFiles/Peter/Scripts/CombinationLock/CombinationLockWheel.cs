7a39d9af934aa295305c4212ad2eba478a7e05d7
gmd1apeterbos <97040824@st.deltion.nl>
2018-03-25 12:11:58 +0200

    Combination lock puzzle

---
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/CombinationLock/CombinationLockWheel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinationLockWheel : TriggerdObjects {
+    public int currentSelected = -1;
+
+    public List<int> combination = new List<int>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void TriggerFunctionality()
+    {
+        Cycle();
+    }
+
+    public void Cycle()
+    {
+        if(currentSelected < 9)
+        {
+            currentSelected++;
+            GetComponent<ObjectDescriptor>().descriptions.Clear();
+            GetComponent<ObjectDescriptor>().descriptions.Add("" + currentSelected);
+        }
+        else if(currentSelected == 9)
+        {
+            currentSelected = 0;
+            GetComponent<ObjectDescriptor>().descriptions.Clear();
+            GetComponent<ObjectDescriptor>().descriptions.Add("" + currentSelected);
+        }
+    }
+}
---
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/CombinationLock/CombinationlockPuzzle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinationlockPuzzle : Puzzle {
+    public CombinationLockWheel myWheel;
+    private bool canCycle = true;
+    public float cycleCooldown = 2;
+
+    public int i;
+
+    public List<TriggerdObjects> trapsLose = new List<TriggerdObjects>();
+	// Use this for initialization
+	void Start () {
+        puzzleManager = FindObjectOfType<PuzzleManager>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CycleWheel();
+	}
+
+    public override void PuzzleTrigger(TriggerdObjects currentObject)
+    {
+        if(myWheel.currentSelected == myWheel.combination[i])
+        {
+            Debug.Log("Correct");
+            i++;
+
+            if(i == myWheel.combination.Count)
+            {
+                puzzleManager.done = true;
+            }
+        }
+        else
+        {
+            Debug.Log("Incorrect");
+            i = 0;
+            //Trigger traps
+            foreach (TriggerdObjects t in trapsLose)
+            {
+                t.TriggerFunctionality();
+            }
+        }
+    }
+
+    public void CycleWheel()
+    {
+        if (canCycle)
+        {
+            StartCoroutine(StartCycle());
+        }
+    }
+
+    private IEnumerator StartCycle()
+    {
+        canCycle = false;
+        myWheel.TriggerFunctionality();
+        yield return new WaitForSeconds(cycleCooldown);
+        canCycle = true;
+    }
+}
