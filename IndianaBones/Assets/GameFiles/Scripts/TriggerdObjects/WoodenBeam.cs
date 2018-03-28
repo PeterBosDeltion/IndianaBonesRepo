2918a93b41a0ed96ee7c152c7e8e43d4e5736898
Kevin Otten <kevin.otten@ziggo.nl>
2018-03-28 16:08:59 +0200

    beams klaar al niet heelemaal als ik wou door probleemen met de code (of het kon niet of het werkte niet)

--- IndianaBones/Assets/GameFiles/Scripts/TriggerdObjects/WoodenBeam.cs
+++ IndianaBones/Assets/GameFiles/Scripts/TriggerdObjects/WoodenBeam.cs
@@ -13,43 +13,48 @@ public class WoodenBeam : TriggerdObjects {
 	
 	public BeamAction state;
 	public float afterTime;
+	// gradual == snelheid van animation tijdes gradual state, 1 is max speed
 	public float gradual;
 	public float instant;
+	public float gradualWaitTime;
 	public Animator beam;
 
 	void Start()
 	{
 		beam = GetComponent<Animator>();
 	}
-
-	void Update()
-	{
-		if(state == BeamAction.Instant)
-		{
-			print("update");
-			// if(beam.)
-			// {
-			// 	beam.SetTrigger("Trigger");
-			// }
-		}
-	}
 	public override void TriggerFunctionality()
 	{
-		beam.SetTrigger("Trigger");
 		if(state == BeamAction.AfterTime)
 		{
-			StartCoroutine(BeamTimer());
+			beam.SetTrigger("Trigger");
+			StartCoroutine(BeamTimer(afterTime));
 		}
 
 		if(state == BeamAction.Gradual)
 		{
-
+			beam.SetTrigger("Trigger");
+			StartCoroutine(BeamTimer(instant));
+		}
+		if(state == BeamAction.Instant)
+		{
+			beam.SetTrigger("Trigger");
+			StartCoroutine(BeamTimer(instant));
 		}
 	}
-	public IEnumerator BeamTimer ()
+	public IEnumerator BeamTimer (float time)
 	{
-		yield return new WaitForSeconds(afterTime);
-		print("Triggerd reeeeeeee");
+		yield return new WaitForSeconds(time);
+		if(state == BeamAction.Gradual)
+		{
+			beam.speed = gradual;
+		}
 		beam.SetTrigger("Trigger");
+		yield return new WaitForSeconds(gradualWaitTime);
+		if(state == BeamAction.Gradual)
+		{
+			print("nu");
+			beam.speed = 1;
+		}
 	}
 }
