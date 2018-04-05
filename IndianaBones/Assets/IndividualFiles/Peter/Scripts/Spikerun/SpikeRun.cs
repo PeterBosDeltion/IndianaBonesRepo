bdc41bc1c878d118a381e277888a1f672153394d
gmd1apeterbos <97040824@st.deltion.nl>
2018-04-05 10:07:19 +0200

    Spike run

---
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/Spikerun/SpikeRun.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeRun : Puzzle {
+
+    public List<TriggerdObjects> spikes = new List<TriggerdObjects>();
+    public float spikeInterval = 2;
+	// Use this for initialization
+	void Start () {
+        puzzleManager = FindObjectOfType<PuzzleManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void PuzzleTrigger(TriggerdObjects currentObject)
+    {
+        puzzleManager.done = true;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            StartPuzzle();
+        }
+    }
+
+    public void StartPuzzle()
+    {
+        StartCoroutine(DeploySpikes());
+    }
+
+    IEnumerator DeploySpikes()
+    {
+        foreach (TriggerdObjects t in spikes)
+        {
+            yield return new WaitForSeconds(spikeInterval);
+
+            t.TriggerFunctionality();
+        }
+    }
+}
---
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/Spikerun/SpikeRunButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeRunButton : TriggerdObjects {
+	// Use this for initialization
+	void Start () {
+        puzzleManager = FindObjectOfType<PuzzleManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void TriggerFunctionality()
+    {
+        puzzleManager.puzzleInsert(this);
+    }
+}
