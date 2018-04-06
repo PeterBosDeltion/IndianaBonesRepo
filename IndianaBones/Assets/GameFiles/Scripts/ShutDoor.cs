b9768d7ab5766d53da51407a54279e2bdfd8f4eb
Kevin Otten <kevin.otten@ziggo.nl>
2018-04-06 17:05:25 +0200

    updatje

---
+++ IndianaBones/Assets/GameFiles/Scripts/ShutDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShutDoor : InteractableObject {
+
+	public bool closed;
+	public void OnTriggerEnter()
+	{
+		if(closed == false)
+		{
+			closed = true;
+			Trigger();
+		}
+	}
+}
