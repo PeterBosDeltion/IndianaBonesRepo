7fe3df76d48e4c39ae404985bb063665cc30661b
Kevin Otten <kevin.otten@ziggo.nl>
2018-04-06 16:36:07 +0200

    skyrim puzzle intergrated

---
+++ IndianaBones/Assets/GameFiles/Scripts/Puzzles/SkyrimPuzzle/RotatingPillar.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPillar : TriggerdObjects {
+
+	public enum Side
+	{
+		side1,
+		side2,
+		side3
+	}
+	public Side side;
+	public int correctState;
+	private bool canMove = true;
+	void Start()
+	{
+		puzzleManager = FindObjectOfType<PuzzleManager>();
+		GetComponent<Animator>().SetInteger("CurentState", (int)side);
+	}
+	public override void TriggerFunctionality()
+	{
+		if(puzzleManager.puzzleList[puzzleNumber].puzzleDone != true && canMove == true)
+		{
+			canMove = false;
+			StartCoroutine(rotationBlock());
+			if(side == Side.side1)
+			{
+				side = Side.side2;
+				GetComponent<Animator>().SetTrigger("Rotate");
+				if(correctState == 2)
+				{
+					puzzleManager.puzzleInsert(this);
+				}
+			}
+			else if(side == Side.side2)
+			{
+				side = Side.side3;
+				GetComponent<Animator>().SetTrigger("Rotate");
+				if(correctState == 3)
+				{
+					print("ya boi");
+					puzzleManager.puzzleInsert(this);
+				}
+			}
+			else if(side == Side.side3)
+			{
+				side = Side.side1;
+				GetComponent<Animator>().SetTrigger("Rotate");
+				if(correctState == 1)
+				{
+					puzzleManager.puzzleInsert(this);
+				}
+			}
+		}
+	}
+
+	public IEnumerator rotationBlock()
+	{
+		yield return new WaitForSeconds(1.5f);
+		canMove = true;
+	}
+}
---
+++ IndianaBones/Assets/GameFiles/Scripts/Puzzles/SkyrimPuzzle/SkyrimPuzzle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyrimPuzzle : Puzzle 
+{
+	public List<bool> finishedParts = new List<bool>();
+
+	void Start()
+	{
+		puzzleManager = FindObjectOfType<PuzzleManager>();
+	}
+	public override void PuzzleTrigger(TriggerdObjects currentObject)
+	{
+		if(currentObject.puzzlePart == 3)
+		{
+			PuzzleCheck();
+		}
+	}
+
+	public void PuzzleCheck()
+	{
+		returnBool = true;
+		foreach (bool b in finishedParts)
+		{
+			if(b == false)
+			{
+				returnBool = false;
+			}
+		}
+		puzzleManager.done = returnBool;
+	}
+}
