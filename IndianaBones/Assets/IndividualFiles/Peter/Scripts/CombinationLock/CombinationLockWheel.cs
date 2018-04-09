7ba474e57c6b2dff87b66aa6cabd008c2aeeacfb
gmd1apeterbos <97040824@st.deltion.nl>
2018-04-09 21:07:55 +0200

    Combinationlock in main scene

--- IndianaBones/Assets/IndividualFiles/Peter/Scripts/CombinationLock/CombinationLockWheel.cs
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/CombinationLock/CombinationLockWheel.cs
@@ -24,17 +24,79 @@ public class CombinationLockWheel : TriggerdObjects {
 
     public void Cycle()
     {
-        if(currentSelected < 9)
+        if(currentSelected < 12)
         {
             currentSelected++;
-            GetComponent<ObjectDescriptor>().descriptions.Clear();
-            GetComponent<ObjectDescriptor>().descriptions.Add("" + currentSelected);
+            //GetComponent<ObjectDescriptor>().descriptions.Clear();
+            //GetComponent<ObjectDescriptor>().descriptions.Add("" + currentSelected);
         }
-        else if(currentSelected == 9)
+        else if(currentSelected == 12)
         {
             currentSelected = 0;
-            GetComponent<ObjectDescriptor>().descriptions.Clear();
-            GetComponent<ObjectDescriptor>().descriptions.Add("" + currentSelected);
+            //GetComponent<ObjectDescriptor>().descriptions.Clear();
+            //GetComponent<ObjectDescriptor>().descriptions.Add("" + currentSelected);
         }
+
+        if(currentSelected == 1)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -15);
+
+        }
+        else if (currentSelected == 2)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -45);
+
+        }
+        else if (currentSelected == 3)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -75);
+
+        }
+        else if (currentSelected == 4)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -105);
+            Debug.Log("4");
+
+        }
+        else if (currentSelected == 5)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -135);
+
+        }
+        else if (currentSelected == 6)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -165);
+
+        }
+        else if (currentSelected == 7)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -195);
+
+        }
+        else if (currentSelected == 8)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -225);
+
+        }
+        else if (currentSelected == 9)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -254);
+
+        }
+        else if (currentSelected == 10)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -285);
+
+        }
+        else if (currentSelected == 11)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, -315);
+
+        }
+        else if (currentSelected == 12)
+        {
+            transform.localRotation = Quaternion.Euler(0, -180, 15);
+        }
+
     }
 }
