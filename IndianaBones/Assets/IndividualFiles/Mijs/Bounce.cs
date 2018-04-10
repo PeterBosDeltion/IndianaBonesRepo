ad79d284cf5ae4b91a392ffaded9f5529b3e0a82
mijs3 <mijs.tenhove@hotmail.com>
2018-04-10 14:04:53 +0200

    bounce

---
+++ IndianaBones/Assets/IndividualFiles/Mijs/Bounce.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	private Vector3 startpos;
+
+	public float forceAmount;
+	public float randomSideForce;
+
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		startpos = transform.position;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			PushUp();
+		}
+	}
+	public void PushUp()
+	{
+		// De Vector3 die straks gerandomized gaat worden.
+		Vector3 randomizer;
+
+		// Random getal ( nul of één ), als hij nul is schiet hem naar links, als hij één is schiet hem naar rechts.
+		int leftOrRight = Random.Range(0, 2);
+		if (leftOrRight == 0)
+		{
+			// Random force naar links.
+			randomizer = new Vector3(Random.Range(randomSideForce, 2 * randomSideForce), 0, 0);
+		}
+		else
+		{
+			// Random force naar rechts.
+			randomizer = new Vector3(Random.Range(-randomSideForce, -(2 * randomSideForce)), 0, 0);
+		}
+
+		// Add de force om hem omhoog te schieten
+		rb.AddForce((Vector3.up + randomizer) * forceAmount, ForceMode.Impulse);
+	}
+}
