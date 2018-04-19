382e28744124a75fc89655967da282074235530f
gmd1apeterbos <97040824@st.deltion.nl>
2018-04-19 10:35:10 +0200

    Audio script

---
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/Audio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : TriggerdObjects {
+    public AudioSource sourceToPlay;
+    private GameObject collisionOther;
+
+    public enum State
+    {
+        Trigger,
+        Interact,
+        Start,
+
+    }
+
+    public State whenToPlay;
+	// Use this for initialization
+	void Start () {
+		if(whenToPlay == State.Start)
+        {
+            sourceToPlay.Play();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("E"))
+        {
+            if(collisionOther != null)
+            {
+                if(collisionOther.tag == "Player")
+                {
+                    if(whenToPlay == State.Interact)
+                    {
+                        sourceToPlay.Play();
+                    }
+                }
+            }
+        }
+	}
+
+    public void OnTriggerStay(Collider other)
+    {
+        collisionOther = other.gameObject;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        collisionOther = null;
+    }
+
+    public override void TriggerFunctionality()
+    {
+        if(whenToPlay == State.Trigger)
+        {
+            sourceToPlay.Play();
+        }
+    }
+}
--- IndianaBones/Assets/IndividualFiles/Peter/Scripts/DartTrapArrow.cs
+++ IndianaBones/Assets/IndividualFiles/Peter/Scripts/DartTrapArrow.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DartTrapArrow : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 		
