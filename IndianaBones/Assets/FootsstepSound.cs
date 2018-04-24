a727bd3c611dd56d4e663366763b3efef19e9a8c
Lajos de Graaf <lajosdegraaf@hotmail.com>
2018-04-24 14:04:06 +0200

    Sound Effects Edition

---
+++ IndianaBones/Assets/FootsstepSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootsstepSound : MonoBehaviour {
+
+    public AudioClip footStep;
+    public AudioSource audioS;
+
+
+	void Start ()
+    {
+		
+	}
+
+    void FootStep()
+    {
+        audioS.PlayOneShot(footStep);
+    }
+}
---
+++ IndianaBones/Assets/KickSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickSound : MonoBehaviour {
+
+    public AudioClip kickSound;
+    public AudioSource audioS;
+
+
+    void Start()
+    {
+
+    }
+
+    void Kicking()
+    {
+        audioS.PlayOneShot(kickSound);
+    }
+}
+
